ba38f7e8073b1c6836340614894e2de6ed6ded60
Max <maxrogerssepulveda@gmail.com>
2020-11-13 20:56:58 -0300

    api dates and base urls implemented

---
+++ src/api.js
@@ -0,0 +1,35 @@
+// BASE URL
+const base_url = `https://api.rawg.io/api/`;
+
+// GETTING THE DATE
+const getCurrentMonth = () => {
+  const month = new Date().getMonth() + 1;
+
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+};
+
+const getCurrentDay = () => {
+  const day = new Date().getDay();
+
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+};
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+// POPULAR GAMES
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
