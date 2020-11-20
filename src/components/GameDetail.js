5ccb0cc2ce8e2078aa991a0e9d56241a1e7416dd
Max <maxrogerssepulveda@gmail.com>
2020-11-20 12:52:33 -0300

    image media resized

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
 
 const GameDetail = () => {
   const history = useHistory();
@@ -41,14 +42,21 @@ const GameDetail = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={game.background_image} alt={game.name} />
+              <img
+                src={smallImage(game.background_image, 1280)}
+                alt={game.name}
+              />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
             </Description>
             <div className="gallery">
               {screen.results.map((screen) => (
-                <img key={screen.id} src={screen.image} alt={screen.id} />
+                <img
+                  key={screen.id}
+                  src={smallImage(screen.image, 1280)}
+                  alt={screen.id}
+                />
               ))}
             </div>
           </Detail>
